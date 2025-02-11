6fa2cd5f54a43e0913b86b6f68e60e4ceafc7e80
Andishe Abbasian <andishdev@gmail.com>
2025-02-11 16:00:16 +0330

    Feat:updated-paginate

--- src/components/table/paginate/index.tsx
+++ src/components/table/paginate/index.tsx
@@ -71,29 +71,22 @@ const PaginateCustom: React.FC<PaginateCustomProps> = ({
           <div className="text-gray-500">رکورد</div>
         </div>
       </div>
-
-      <ReactPaginate
-        nextLabel={
-          <div className="border-[0.5px] border-neutral-5 bg-neutral-3 rounded-[6px] py-2 px-3 text-[16px]">
-            بعدی
-          </div>
-        }
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={2}
-        pageCount={totalPage}
-        forcePage={forcePage}
-        previousLabel={
-          <div className="border-[0.5px] border-neutral-5 bg-neutral-3 rounded-[6px] py-2 px-3 text-[16px]">
-            قبلی
-          </div>
-        }
-        activeClassName="activePage bg-primary-500 border-primary-500 [&>a]:text-white text-[16px]"
-        pageClassName="box-border bg-neutral-3 [&>a]:w-[40px] [&>a]:h-[40px] border-[0.5px] border-neutral-5 [&>a]:p-2 [&>a] rounded-[6px] [&>a]:flex [&>a]:justify-center [&>a]:items-center cursor-pointer text-[16px]"
-        className="pagination [&>li]:text-neutral-10 [&>li]:!mx-[2px]"
-        previousClassName="page-link"
-        disabledClassName="!text-neutral-7"
-      />
-    </div>
+        <ReactPaginate
+          nextLabel="بعدی"
+          previousLabel="قبلی"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          pageCount={totalPage}
+          forcePage={forcePage}
+          renderOnZeroPageCount={null}
+          containerClassName="pagination [&>li]:text-neutral-10 [&>li]:!mx-[2px]"
+          pageClassName="box-border bg-neutral-3 [&>a]:w-[40px] [&>a]:h-[40px] border-[0.5px] border-neutral-5 [&>a]:p-2 [&>a] rounded-[6px] [&>a]:flex [&>a]:justify-center [&>a]:items-center cursor-pointer text-[16px]"
+          activeClassName="activePage bg-primary-500 border-primary-500 [&>a]:text-white text-[16px]"
+          previousClassName="page-link border-[0.5px] border-neutral-5 bg-neutral-3 rounded-[6px] py-2 px-3 text-[16px]"
+          nextClassName="page-link border-[0.5px] border-neutral-5 bg-neutral-3 rounded-[6px] py-2 px-3 text-[16px]"
+          disabledClassName="!text-neutral-7"
+        />
+      </div>
   );
 };
 
